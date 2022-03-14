ed828d83d5801a38993ad8e2011ac91ed90fc4f2
sm19043 <sm19043@josai.ac.jp>
2022-03-14 17:32:55 +0900

    3/14

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,10 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', function() {
+    return view('top/index');
+});
+
 
 Route::get('/deckrecipe', 'DeckrecipeController@index'); //デッキレシピのトップ画面
 
